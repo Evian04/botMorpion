3789879fea44b72d7950a5d053e07eea12852809
Evan <118688666+Evian04@users.noreply.github.com>
2022-11-28 15:47:27 +0100

    finished base functions

--- src/module/player.py
+++ src/module/player.py
@@ -1,4 +1,10 @@
 class Player:
-    def __init__(self, is_bot: bool, name: str): pass
-    def get_is_bot(self) -> bool: pass
-    def get_name(self) -> str: pass
+    def __init__(self, is_bot: bool, name: str):
+        self.is_bot = is_bot
+        self.name = name
+
+    def get_is_bot(self) -> bool:
+        return self.is_bot
+
+    def get_name(self) -> str:
+        return self.name
