e8c055e5e2489932d73bdc3dd2f14962ba9af359
Evan <118688666+Evian04@users.noreply.github.com>
2022-12-03 14:42:03 +0100

    function  implemented

--- src/morpion.py
+++ src/morpion.py
@@ -59,7 +59,29 @@ class Morpion:
         # return if the grid is full
         return len(self.get_empty_cell()) == 0
 
-    def is_winner(self, player_name: str = "Any") -> bool: pass
+    def is_winner(self, player_name: str = "Any") -> bool:
+        # This function return if the player `player_name` won, or if `player_name == "Any"`, if any player won
+        list_threesomes = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+        for threesome in list_threesomes:
+            if self.test_threesome(threesome, player_name):
+                return True
+        
+        return False
+    
+    def test_threesome(self, threesome: list, player_name: str) -> bool:
+        # This function return if the value of three cells are equals to the sign of a certain player or if their are just equal if `player_name == "Any"`
+        if player_name == "Any":
+            ref_value = self.content[threesome[0]]
+        
+        else:
+            ref_value = self.get_player(player_name).get_sign()
+        
+        for index_cell in threesome:
+            if self.content[index_cell] != ref_value:
+                return False
+        
+        return True
 
     def display_grid(self):
         # This function display the grid in the terminal
