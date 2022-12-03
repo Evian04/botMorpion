f5116aa76f711f53fbeba68c05cdac88fd8470fe
Evan <118688666+Evian04@users.noreply.github.com>
2022-12-03 10:05:57 +0100

    function  implemented

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from src.morpion import Morpion
-from src.bot import Bot
 from src.player import Player
 
 
-game = Morpion(Player(False, "O", "user"), Player(True, "X", "bot"))
+name_player_1 = "user"
+name_player_2 = "bot"
+game = Morpion(Player(False, "O", name_player_1), Player(True, "X", name_player_2))
 
 while not (game.is_full() or game.is_winner()):
     game.display_board()
-    game.set_digit("user", input("In which cell do you wonna play ?"))
+    for name_player in [name_player_1, name_player_2]:
+        game.play(name_player)
--- src/morpion.py
+++ src/morpion.py
@@ -1,4 +1,5 @@
 from src.player import Player
+from src.bot import Bot
 
 
 class Morpion:
@@ -12,6 +13,25 @@ class Morpion:
         self.player_1 = player_1
         self.player_2 = player_2
         self.grid = [str(d + 1) for d in range(9)]
+        
+        if self.player_1.get_is_bot or self.player_2.get_is_bot():
+            self.bot == Bot()
+    
+    def play(self, player_name: str):
+        player = self.get_player(player_name)
+        if player.get_is_bot():
+            self.set_digit(player_name, self.bot.get_play_index(self))
+        
+        else:
+            self.set_digit(player_name, self.request_user)
+    
+    def request_user(self) -> int:
+        user_input = input("Enter the place you want to play : ")
+        if not user_input.is_digit() or not int(user_input) - 1 in range(9):
+            print("You must enter a digit between 1 and 9 inclusive !")
+            return self.request_user()
+        
+        return int(user_input) - 1
 
     def set_digit(self, player_name: str, index: int):
         if not index in range(9): # If `index` isn't between 0 and 8
@@ -21,6 +41,14 @@ class Morpion:
 
     def get_content(self) -> list[str]:
         return self.grid
+    
+    def get_empty_cell(self) -> list:
+        empty_cells = []
+        for i in range(9):
+            if self.grid[i].isdigit():
+                empty_cells.append(i)
+        
+        return empty_cells
 
     def get_player(self, player_name: str) -> Player:
         """ Return some information about a player according to his name """
